8b2a48b16e21b9e7ff1da042bc0f498d5ce86657
taewook <taewook@NHS514-08.ccsd.int>
2019-01-25 11:30:31 -0500

    1/25/2019

--- src/Rectangle/Prism.java
+++ src/Rectangle/Prism.java
@@ -18,6 +18,36 @@ public class Prism extends Rectangle {
 		else
 			H = 1.0;
 	}
+	
+	public Prism(Prism other)
+	{
+		super(other.getLength(), other.getWidth());
+		this.H = other.H;
+	}
+	
+	public String toString()
+	{
+		return super.toString() + ", H = " + this.H;
+	}
 
-
+	// Method inherited from Rectangle
+	// getLengh(), getWidth(), *toString()
+	// We would need to override these also,
+	// since they are not exactly appropriate for a Prism.
+	// *area(), *perimeter(), *diagonal()
+	
+	// getHeight()
+	public double getHeight()
+	{
+		return H;
+	}
+	
+	// volume()
+	public double volume()
+	{
+		return super.area() * H;
+	}
+	
+	
+	
 }
--- src/Rectangle/RectangleTester.java
+++ src/Rectangle/RectangleTester.java
@@ -6,5 +6,9 @@ public class RectangleTester {
 		Square S1 = new Square(10);
 		System.out.println("Square.area(): " + S1.area());
 		
+		Prism P = new Prism(3, 4, 5);
+		
+		System.out.println("Prism P: " + P);
+		
 	}
 }
