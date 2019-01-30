59d4c0987620172a5f6d84ec5004afbffd5e8896
taewook <taewook@NHS514-08.ccsd.int>
2019-01-30 11:47:39 -0500

    1/30/2019

---
+++ src/ArrayList/ArrayListTester.java
@@ -0,0 +1,42 @@
+package ArrayList;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class ArrayListTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList Objs = new ArrayList();
+		
+		// Instantiation of an ArrayList<String> object.
+		ArrayList<String> Names = new ArrayList<String>();
+		
+		// Instatiation of an ArrayList<Integer> object.
+		ArrayList<Integer> Nums = new ArrayList<Integer>();
+		
+		// Adding objects to an ArrayList can be done
+		// in two ways.
+		
+		Names.add("Joe");
+		Names.add("Bob");
+		Names.add("Mary");
+		Names.add("Timmy");
+		
+		System.out.println("Names: " + Names);
+		System.out.println("We have " + Names.size() + " names.");
+		
+		/*
+ 			Names: [Joe, Bob, Mary, Timmy]
+		  	We have 4 names
+		*/
+		
+		// Names.set
+		
+		
+		
+		
+	}
+
+}
