c79dea71285ded97585678365c6f07d392e17b31
taewook <taewook@NHS514-08.ccsd.int>
2019-02-08 11:29:19 -0500

    2/8/2019

--- src/StatListTester/StatList.java
+++ src/StatListTester/StatList.java
@@ -1,14 +1,20 @@
 package StatListTester;
 import java.util.ArrayList;
+import java.lang.Math;
 import java.util.List;
 
 public class StatList implements Liist<Double>{
-	
+
 	private ArrayList<Double> L;
-	
+
 	public StatList()
 	{
 		L = new ArrayList<Double>();
+	}
+	
+	public String toString()
+	{
+		return L.toString();
 	}
 
 	public boolean isEmpty() 
@@ -59,17 +65,81 @@ public class StatList implements Liist<Double>{
 		return null;
 	}
 
-	// find
-	// count
-	// highest
+	// find uses Object.equals()
+	public int find(Double K)
+	{
+		for (int i = 0; i < L.size(); i++)
+			if(L.get(i).equals(K))
+				return i;
+		return -1;
+	}
+
+	// count value, uses Object.equals()
+	// Returns the number of times K is found in list L.
+	public int count(Double K)
+	{
+		int c = 0;
+		for (int i = 0; i < L.size(); i++)
+			if(L.get(i).equals(K))
+				c++;
+		return c;
+	}
+
+	// highest, uses Comparable.compareTo()
+	// Works with all classes that implement Comparable
+	// Returns the position of the highest value in L.
+	public int highest()
+	{
+		int p = 0;
+		for(int i = 0; i < L.size(); i++)
+			if(L.get(p).compareTo(L.get(i)) > 0)
+				p = i;
+		return p;
+	}
+
 	// sort
+	public void sort()
+	{
+		for(int n = 1; n <= L.size(); n++)
+			for(int i = 0; i < L.size()-1; i++)
+				if(L.get(i).compareTo(L.get(i+1)) > 0)
+					L.set(i, L.set(i+1, L.get(i)));
+	}
+
 	// summation
+	public Double summation()
+	{
+		double total = 0.0;
+		for(int i = 0; i < L.size(); i++)
+			total += L.get(i).doubleValue();
+		return new Double(total);
+	}
+	
 	// mean
+	public Double mean()
+	{
+		return new Double((summation().doubleValue())/(double)L.size());
+	}
+	
 	// standard deviation
+	// The square root of, the sum of the squares of
+	// the differences between each element and the mean
+	// which is divided by n-1.
+	public Double standardDeviation()
+	{
+		double sum = 0.0;
+		
+		for(int i = 0; i < L.size(); i++)
+			sum += ((L.get(i) - mean().doubleValue())*(L.get(i) - mean().doubleValue()));
+		sum /= (double)(L.size()-1);
+		sum = Math.sqrt(sum);
+		return new Double(sum);
+	}
+	
 	// median (without sorting list L, make a auxiliary list)
 	// max value
 	// min value
-	
-	
-	
+
+
+
 }
--- src/StatListTester/StatListTester.java
+++ src/StatListTester/StatListTester.java
@@ -3,8 +3,28 @@ package StatListTester;
 public class StatListTester {
 
 	public static void main(String[] args) {
+		StatList theList = new StatList();
 		
-
+		System.out.println("theList.size() = " + theList.size() );
+		
+		theList.add(new Double(5));
+		theList.add(new Double(20));
+		theList.add(new Double(4));
+		theList.add(new Double(15));
+		theList.add(new Double(4));
+		theList.add(new Double(1));
+		
+		System.out.println("L: " + theList);
+		
+		Double Sum = theList.summation();
+		System.out.println("Sum: " + Sum); //49
+		
+		Double Mean = theList.mean();
+		System.out.println("Mean: " + Mean);
+	
+		Double Dev = theList.standardDeviation();
+		System.out.println("Dev = " + Dev);
+		
 	}
 
 }
