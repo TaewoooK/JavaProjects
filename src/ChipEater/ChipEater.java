e68ed1222fd02f5f80a3d6df7542d167555fd8ca
taewook <eddiekim1125@gmail.com>
2019-06-11 11:33:20 -0400

    Create start menu

--- src/ChipEater/ChipEater.java
+++ src/ChipEater/ChipEater.java
@@ -11,6 +11,11 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 	private final int apHeight = 320;
 	private final int min = 20;
 
+	private Button start;
+	private Button help;
+	private Button quit;
+	boolean startClicked = false;
+
 	private Image doritos;
 	private Checkbox doritosBox;
 	private Image lays;
@@ -38,6 +43,8 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 	private Image border;
 
 	String chip = "";
+	String max = "";
+
 	CheckboxGroup checkGroup = new CheckboxGroup();
 	int Jwidth = 70;
 
@@ -48,67 +55,94 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 	boolean white;
 
 	int chipLocX = 200;
-	int chipLocY = 225;
+	int chipLocY = 210;
+
+	Image[] fat = new Image[7];
+	int i = 0;
 
 	public void init()
 	{ 
+		start = new Button();
+		start.setLabel("START");
+
+		fat[0] = getImage(getDocumentBase(), "B1.png");
+		fat[1] = getImage(getDocumentBase(), "B2.png");
+		fat[2] = getImage(getDocumentBase(), "B3.png");
+		fat[3] = getImage(getDocumentBase(), "B4.png");
+		fat[4] = getImage(getDocumentBase(), "B5.png");
+		fat[5] = getImage(getDocumentBase(), "B6.png");
+		fat[6] = getImage(getDocumentBase(), "B7.png");
+
 		dChipShow = false;
 		lChipShow = false;
 		vStickShow = false;
 
 		white = true;
-//hi
+
 		this.setSize(new Dimension(650, 450));
+
+		add(start);
+		// add(help);
+		// add(quit);
 
 		good = new Font("Elephant", Font.BOLD, 18);
 
-		chipLabel = getImage(getDocumentBase(), "Chips Label.png");
-		selChip = getImage(getDocumentBase(), "Select Chip.png");
-		border = getImage(getDocumentBase(), "Border.png");
+		if(startClicked == true)
+		{
+			chipLabel = getImage(getDocumentBase(), "Chips Label.png");
+			selChip = getImage(getDocumentBase(), "Select Chip.png");
+			border = getImage(getDocumentBase(), "Border.png");
 
-		eat = new Button();
-		eat.setLabel("Open!");
-		eat.addActionListener(this);
-		add(eat);
+			eat = new Button();
+			eat.setLabel("Open");
+			eat.addActionListener(this);
+			add(eat);
 
-		close = new Button();
-		close.setLabel("Close!");
-		close.addActionListener(this);
-		add(close);
+			close = new Button();
+			close.setLabel("Close");
+			close.addActionListener(this);
+			add(close);
 
-		goChip = new Button();
-		goChip.setLabel("-->");
-		goChip.addActionListener(this);
-		add(goChip);
+			goChip = new Button();
+			goChip.setLabel("------>");
+			goChip.addActionListener(this);
+			add(goChip);
 
-		joyel = getImage(getDocumentBase(), "joyel.png");
-		joyelEat = getImage(getDocumentBase(), "joyelOpen.png");
+			joyel = getImage(getDocumentBase(), "joyel.png");
+			joyelEat = getImage(getDocumentBase(), "joyelOpen.png");
 
-		temp = joyel;
+			temp = joyel;
 
-		dChip = getImage(getDocumentBase(), "dChip.png");
-		lChip = getImage(getDocumentBase(), "lChip.PNG");
-		vStick = getImage(getDocumentBase(), "vStick.png");
+			dChip = getImage(getDocumentBase(), "dChip.png");
+			lChip = getImage(getDocumentBase(), "lChip.PNG");
+			vStick = getImage(getDocumentBase(), "vStick.png");
 
-		doritos = getImage(getDocumentBase(), "doritos.png");
-		doritosBox = new Checkbox("Doritos", checkGroup, false);
-		add(doritosBox);		
-		doritosBox.addItemListener(this);
+			doritos = getImage(getDocumentBase(), "doritos.png");
+			doritosBox = new Checkbox("Doritos", checkGroup, false);
+			add(doritosBox);		
+			doritosBox.addItemListener(this);
 
-		lays = getImage(getDocumentBase(), "lays.png");
-		laysBox = new Checkbox("Lays", checkGroup, false);
-		add(laysBox);
-		laysBox.addItemListener(this);
+			lays = getImage(getDocumentBase(), "lays.png");
+			laysBox = new Checkbox("Lays", checkGroup, false);
+			add(laysBox);
+			laysBox.addItemListener(this);
 
-		veggie = getImage(getDocumentBase(), "veggie.png");
-		veggieBox = new Checkbox("Veggie Straws", checkGroup, false);
-		add(veggieBox);
-		veggieBox.addItemListener(this);
-
+			veggie = getImage(getDocumentBase(), "veggie.png");
+			veggieBox = new Checkbox("Veggie Straws", checkGroup, false);
+			add(veggieBox);
+			veggieBox.addItemListener(this);
+		}
 	}
 
 	public void actionPerformed(ActionEvent e) 
 	{
+		if(e.getSource() == start)
+		{
+			startClicked = true;
+			this.init();
+			repaint();
+		}
+		
 		if(e.getSource() == eat)
 		{	
 			joyel = joyelEat;
@@ -119,24 +153,30 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 		{
 			joyel = temp;
 
-			if(checkGroup.getSelectedCheckbox() == doritosBox)
+			if(checkGroup.getSelectedCheckbox() == doritosBox && chipLocX == 380)
 			{
 				chipLocX = 200; 
 				dChipShow = false;
+				if(i < 6)
+					i++;
 				repaint();
 			}
 
-			if(checkGroup.getSelectedCheckbox() == laysBox)
+			if(checkGroup.getSelectedCheckbox() == laysBox && chipLocX == 370)
 			{
 				chipLocX = 200; 
 				lChipShow = false;
+				if(i < 6)
+					i++;
 				repaint();
 			}
 
-			if(checkGroup.getSelectedCheckbox() == veggieBox)
+			if(checkGroup.getSelectedCheckbox() == veggieBox && chipLocX == 360)
 			{
 				chipLocX = 200; 
 				vStickShow = false;
+				if(i > 0)
+					i--;
 				repaint();
 			}
 
@@ -149,19 +189,19 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 			{
 				if(checkGroup.getSelectedCheckbox() == doritosBox)
 				{
-					chipLocX = 280; 
+					chipLocX = 380; 
 					repaint();
 				}
 
 				if(checkGroup.getSelectedCheckbox() == laysBox)
 				{
-					chipLocX = 270; 
+					chipLocX = 370; 
 					repaint();
 				}
 
 				if(checkGroup.getSelectedCheckbox() == veggieBox)
 				{
-					chipLocX = 260; 
+					chipLocX = 360; 
 					repaint();
 				}
 			}
@@ -204,60 +244,85 @@ public class ChipEater extends Applet implements ActionListener, ItemListener{
 		}
 	}
 
-	public void paint(Graphics g) {  
-
+	public void paint(Graphics g) 
+	{  
 		g.drawImage(border, -20, -20, this);
 
-		g.setFont(good);
-		g.drawString(chip, 380, 50);
+		start.setLocation(325, 225);
 
-		g.drawImage(chipLabel, 0, 0, this);
-		g.drawImage(selChip, 55, 0, this);
-
-		eat.setSize(60, 60);
-		eat.setFont(good);
-		eat.setLocation(450, 70);
-
-		close.setSize(60, 60);
-		close.setFont(good);
-		close.setLocation(510, 70);
-
-		goChip.setSize(75, 50);
-		goChip.setFont(good);
-		goChip.setLocation(200, 150);
-
-		g.setColor(Color.blue);
-		g.fillOval(300, 215, Jwidth, 200); 
-		g.drawImage(joyel, 269, 100, this);
-
-		g.drawImage(doritos, 75, 70, this);  
-		doritosBox.setLocation(72, 140);
-
-		g.drawImage(lays, 62, 170, this);  
-		laysBox.setLocation(77, 240);
-
-		g.drawImage(veggie, 72, 270, this);  
-		veggieBox.setLocation(60, 350);
-
-		if(dChipShow == true)
+		if(startClicked == true)
 		{
-			g.drawImage(dChip, chipLocX, chipLocY, this);
-			white = false;
-		}
-		else if(lChipShow == true)
-		{
-			g.drawImage(lChip, chipLocX, chipLocY, this);
-			white = false;
-		}
-		else if(vStickShow == true)
-		{
-			g.drawImage(vStick, chipLocX, chipLocY, this);
-			white = false;
-		}
-		else
-		{
-			g.setColor(Color.white);
-			g.fillOval(200, 225, 50, 50);
+			g.setFont(good);
+			g.drawString(chip, 380, 50);
+
+			g.drawImage(chipLabel, 0, 0, this);
+			g.drawImage(selChip, 55, 0, this);
+
+			eat.setSize(60, 60);
+			eat.setFont(good);
+			eat.setLocation(200, 70);
+
+			close.setSize(60, 60);
+			close.setFont(good);
+			close.setLocation(260, 70);
+
+			goChip.setSize(75, 50);
+			goChip.setFont(good);
+			goChip.setLocation(200, 150);
+
+			g.drawImage(fat[i], 240, 100, this);
+			g.drawImage(joyel, 370, 80, this);
+
+			g.drawImage(doritos, 75, 70, this);  
+			doritosBox.setLocation(72, 140);
+
+			g.drawImage(lays, 62, 170, this);  
+			laysBox.setLocation(77, 240);
+
+			g.drawImage(veggie, 72, 270, this);  
+			veggieBox.setLocation(60, 350);
+
+			if(dChipShow == true)
+			{
+				g.drawImage(dChip, chipLocX, chipLocY, this);
+				white = false;
+			}
+			else if(lChipShow == true)
+			{
+				g.drawImage(lChip, chipLocX, chipLocY, this);
+				white = false;
+			}
+			else if(vStickShow == true)
+			{
+				g.drawImage(vStick, chipLocX, chipLocY, this);
+				white = false;
+			}
+			else
+			{
+				g.setColor(Color.white);
+				g.fillOval(200, 225, 50, 50);
+			}
+
+			if(i == 6)
+			{
+				max = "I am full";
+				g.setColor(Color.black);
+				g.setFont(good);
+				g.drawString(max, 285, 220);
+			}
+
+			if(i == 0 && !(vStickShow == true || lChipShow == true || dChipShow == true))
+			{
+				max = "Feed me junk chips";
+				g.setColor(Color.black);
+				g.setFont(good);
+				g.drawString(max, 190, 220);
+			}
 		}
 	}  
+
+	public void waitEat() throws InterruptedException
+	{
+		Thread.sleep(500);
+	}
 }
